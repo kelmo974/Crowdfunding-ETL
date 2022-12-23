c0ab8919d1d225998efe561f0556ea0d1b0d4863
Kellen <kellen.finn@gmail.com>
2022-12-23 13:59:09 -0500

    adding ERD itself + .png, updated schema, added contraints, various support file updates

---
+++ challenge/add_backers_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "backers" (
+    "backer_id" varchar(5)  NOT NULL,
+    "cf_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_backers" PRIMARY KEY (
+        "backer_id"
+     )
+);
+
+ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "Campaign" ("cf_id");
